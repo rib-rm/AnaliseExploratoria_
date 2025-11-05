6a2c331b84b9a98100d56949d0978b1d276939c1
rib-rm <ribeiro.mq.rafael@gmail.com>
2025-11-05 10:04:20 -0300

    Primeira versão e arquivo base

--- Atividade Pratica 2.R
+++ Atividade Pratica 2.R
@@ -1,1 +1,16 @@
 # Questão 1
+
+# Commit é uma operação de atualização do código para o repositório.
+
+# Questão 2 - Criado
+
+# Questão 3 - Clonado
+
+# Questão 4
+
+library(tidyverse)
+library(readxl)
+setwd("/home/aluno/Área de Trabalho/AnaliseExploratoria_")
+
+base = read_excel("Base_trabalho.xlsx")
+
